091fa6ca79320d1ea52284ea3e53ebdc3710c89a
claudio moreno <maderas.nativo.frutillar@gmail.com>
2025-08-06 18:24:14 +0000

    regresa hasta antes de implementar el menu desplegable del boton menu, a

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -4,81 +4,46 @@ import * as React from "react"
 import Link from "next/link"
 import {
     NavigationMenu,
-    NavigationMenuContent,
     NavigationMenuItem,
     NavigationMenuLink,
     NavigationMenuList,
-    NavigationMenuTrigger,
     navigationMenuTriggerStyle,
   } from "@/components/ui/navigation-menu"
-import { cn } from "@/lib/utils"
-
-const categories = [
-    { title: "Tablas para Servir", href: "/tienda?categoria=tablas-para-servir" },
-    { title: "Regalos Personalizados", href: "/tienda?categoria=regalos-personalizados"},
-    { title: "Tablas de Cocina", href: "/tienda?categoria=tablas-de-cocina" },
-    { title: "Platos y Accesorios", href: "/tienda?categoria=platos-y-accesorios"},
-    { title: "Empresas y Regalos Corporativos", href: "/tienda?categoria=empresas" },
-    { title: "Tablas de Picoteo", href: "/tienda?categoria=tablas-de-picoteo" },
-    { title: "Despedida de Soltera", href: "/tienda?categoria=despedida-de-soltera" },
-];
 
 export function MainNav() {
   return (
     <NavigationMenu>
       <NavigationMenuList>
         <NavigationMenuItem>
-          <Link href="/" asChild>
+          <Link href="/" passHref>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
               Home
             </NavigationMenuLink>
           </Link>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <Link href="/about" asChild>
+          <Link href="/about" passHref>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
               Sobre Nosotros
             </NavigationMenuLink>
           </Link>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Tienda</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <Link
-                    href="/tienda"
-                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                  >
-                    <div className="mb-2 mt-4 text-lg font-medium font-headline">
-                      Todos los Productos
-                    </div>
-                    <p className="text-sm leading-tight text-muted-foreground">
-                      Explora nuestra colección completa de artesanías en madera.
-                    </p>
-                  </Link>
-                </NavigationMenuLink>
-              </li>
-              {categories.map((component) => (
-                <ListItem
-                  key={component.title}
-                  title={component.title}
-                  href={component.href}
-                />
-              ))}
-            </ul>
-          </NavigationMenuContent>
+           <Link href="/tienda" passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Tienda
+            </NavigationMenuLink>
+          </Link>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <Link href="/empresas" asChild>
+          <Link href="/empresas" passHref>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
               Empresas
             </NavigationMenuLink>
           </Link>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <Link href="/contacto" asChild>
+          <Link href="/contacto" passHref>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
               Contacto
             </NavigationMenuLink>
@@ -88,30 +53,3 @@ export function MainNav() {
     </NavigationMenu>
   )
 }
-
-const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, href, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <Link
-          href={href!}
-          ref={ref}
-          className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className
-          )}
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
-        </Link>
-      </NavigationMenuLink>
-    </li>
-  )
-})
-ListItem.displayName = "ListItem"
