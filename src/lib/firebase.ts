fa0ce03f7a9bf555e9ecd55d855aea3733afac31
claudio moreno <maderas.nativo.frutillar@gmail.com>
2025-08-07 19:02:54 +0000

    sigue con el mismo error de poder subir productos o eliminar productos,

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -9,7 +10,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyC0heKAl49bE49y8vadmp4cqa2JinFunUo",
   authDomain: "claudio-madera.firebaseapp.com",
   projectId: "claudio-madera",
-  storageBucket: "claudio-madera.appspot.com",
+  storageBucket: "claudio-madera.firebasestorage.app",
   messagingSenderId: "876259118187",
   appId: "1:876259118187:web:b356dcdbf551ea8941752e",
   measurementId: "G-G74ZRM4JQD"
