928f604aee049361c0eacd0bf72345097b2c5181
claudio moreno <maderas.nativo.frutillar@gmail.com>
2025-08-08 03:46:08 +0000

    Tu proyecto Nombre del proyecto CLAUDIO-MADERA ID del proyecto claudio-m

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyC0heKAl49bE49y8vadmp4cqa2JinFunUo",
+  authDomain: "claudio-madera.firebaseapp.com",
+  projectId: "claudio-madera",
+  storageBucket: "claudio-madera.appspot.com",
+  messagingSenderId: "876259118187",
+  appId: "1:876259118187:web:b356dcdbf551ea8941752e",
+  measurementId: "G-G74ZRM4JQD"
 };
 
 // Initialize Firebase
