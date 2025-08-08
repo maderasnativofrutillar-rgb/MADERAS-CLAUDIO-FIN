632b36c31dac69bf93d9393d0d5dbfd28df98b5c
claudio moreno <maderas.nativo.frutillar@gmail.com>
2025-08-08 16:07:01 +0000

    revisa Error loading webview: Error: Could not register service worker:

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import 'dotenv/config';
 import type {NextConfig} from 'next';
 
@@ -8,6 +9,19 @@ const nextConfig: NextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline';",
+          },
+        ],
+      },
+    ];
   },
   images: {
     remotePatterns: [
